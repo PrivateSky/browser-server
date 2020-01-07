9aafabe2f2cc6334161635011695ebd11fd37171
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-01-07 09:47:41 +0200

    cleanup + refactoring

--- bin/build.js
+++ bin/build.js
@@ -1,13 +1,6 @@
 const browserify = require("browserify");
-const fs = require("fs");
 let b = browserify();
-b.require("../browserfs-dist",{expose:"browserfs-dist"});
-b.require("../adler32",{expose:"adler32"});
-b.require("../pskcrypto",{expose:"pskcrypto"});
-b.require("../pskwebfs",{expose:"fs"});
+const fs = require("fs");
 b.require("../swarmutils",{expose:"swarmutils"});
-b.require("../bar",{expose:"bar"});
-b.require("../psk-http-client",{expose:"psk-http-client"});
-b.require("../edfs-brick-storage",{expose:"edfs-brick-storage"});
 b.add("webroot/swEngine/index.js");
 b.bundle().pipe(fs.createWriteStream('./webroot/sw-browserified.js'))
--- webroot/swEngine/index.js
+++ webroot/swEngine/index.js
@@ -1,133 +1,11 @@
-const STORAGE_PROVIDER = "EDFSBrickStorage";
-const STORAGE_URL = "http://192.168.103.149:9091";
 const Middleware = require("./utils/Middleware").Middleware;
 const ChannelsManager = require("./utils/ChannelsManager").getChannelsManager();
 const UtilFunctions = require("./utils/utilFunctions").UtilFunctions;
-//const Filer = require("./utils/filer/Filer").Filer;
-//Filer.init(STORAGE_PROVIDER, STORAGE_URL);
-const Filer = {
-    extractFolder: function () {
-        console.log("Not implemented");
-    },
-    addFolder: function () {
-        console.log("Not implemented");
-    }
-
-
-}
-const fs = require("fs");
 
 // create a global ref
 let server = new Middleware();
-//
-// server.get("/vmq/:channelId", function (req, res, next) {
-//     console.log(req.params);
-//     next();
-// });
-//
-// server.get("/vmq/:channelName", function (req, res, next) {
-//     console.log(req.params);
-//     res.json({params:req.params});
-// });
 
-// server.get("/file", function (req, res, next) {
-//
-//     let timestamp  = Date.now();
-//     let folderName = "rafa-"+-timestamp;
-//     fs.mkdir(folderName, (err) => {
-//         if (err) throw err;
-//
-//         console.log("FILE request");
-//
-//         let promise = Promise.resolve();
-//
-//
-//         promise = promise.then(() => {
-//             return new Promise((resolve, reject) => {
-//                 fs.writeFile(folderName + '/message.txt', "Lorem ipsum", (err) => {
-//                     if (err) reject(err);
-//                     else {
-//                         resolve();
-//                     }
-//                 });
-//             });
-//         });
-//
-//
-//         promise = promise.then(() => {
-//             return new Promise((resolve, reject) => {
-//                 fs.access(folderName + '/message.txt', (err, stats) => {
-//                     if (err) reject(err);
-//                     else {
-//                         console.log(stats);
-//                         resolve(stats);
-//                     }
-//                 });
-//             });
-//         });
-//
-//         promise = promise.then(()=>{
-//             return new Promise((resolve,reject)=>{
-//                 Filer.addFolder(folderName, (err, digest)=>{
-//                     if(err){
-//                         reject(err);
-//                     }
-//                     else
-//                     {   console.log("Done");
-//
-//                         resolve(digest);
-//                     }
-//                 })});
-//         });
-//
-//         promise = promise.then(() => {
-//             return new Promise((resolve, reject) => {
-//                 Filer.extractFolder(folderName, (err) => {
-//                     if (err) {
-//                         reject(err);
-//                     }
-//                     else {
-//                         resolve("Finished");
-//                     }
-//                 });
-//             });
-//         })
-//
-//         promise = promise.then((status)=>{
-//             console.log(status, folderName);
-//         })
-//
-//
-//     });
-//
-//     //res.attachment("/file/rafa");
-// });
-
-
-// server.post("/vmq/:channelId", function(req, res, next){
-//     if(typeof req.body === "object"){
-//         res.status(200).send({status:"ok"});
-//     }
-//     else{
-//         res.status(200).send({status:"ok"});
-//     }
-// });
-//
-// server.post("/upload", function(req, res, next){
-//     if(req.body.attachment instanceof  File){
-//         res.status(200).send({status:"File upload completed"});
-//     }
-//     else{
-//         res.status(500).send({status:"File upload failed"});
-//     }
-// });
-
-
-/*
-* just adding the event listener to catch all the requests
-*/
-
-server.put("/create-channel/:channelName", function (req, res) {
+function createChannelHandler (req, res) {
     ChannelsManager.createChannel(req.params.channelName, function (err) {
         if (err) {
             res.status(err.code || 500);
@@ -137,17 +15,18 @@ server.put("/create-channel/:channelName", function (req, res) {
         }
         res.end();
     });
-});
+}
 
-server.post("/forward-zeromq/:channelName", function(req, res){
+function forwardMessageHandler(req, res){
     ChannelsManager.forwardMessage(req.params.channelName,function(err){
         if(err){
             res.status(err.code || 500);
         }
         res.end();
     });
-});
-server.post("/send-message/:channelName", function (req, res) {
+}
+
+function sendMessageHandler (req, res) {
 
     UtilFunctions.prepareMessage(req, function (err, bodyAsBuffer) {
 
@@ -167,10 +46,9 @@ server.post("/send-message/:channelName", function (req, res) {
         }
 
     })
+}
 
-});
-
-server.get("/receive-message/:channelName", function (req, res) {
+function receiveMessageHandler (req, res) {
     ChannelsManager.receiveMessage(req.params.channelName, function (err, message) {
         if (err) {
             res.status(err.code || 500);
@@ -188,7 +66,16 @@ server.get("/receive-message/:channelName", function (req, res) {
         }
         res.end();
     });
-});
+}
+
+/*
+* just adding the event listener to catch all the requests
+*/
+
+server.put("/create-channel/:channelName", createChannelHandler);
+server.post("/forward-zeromq/:channelName", forwardMessageHandler);
+server.post("/send-message/:channelName", sendMessageHandler);
+server.get("/receive-message/:channelName", receiveMessageHandler);
 
 /*
 * if no previous handler response to the event it means that the url doesn't exit
